3e1a5a23aac3228c1e7b2d4b7dde394e0ace1930
zenbenten <joshuaelsner97@gmail.com>
2026-04-11 22:09:32 -0400

    Adjust scheduled reset for EST

--- db/013_schedule_weekly_reset.sql
+++ db/013_schedule_weekly_reset.sql
@@ -4,6 +4,6 @@ CREATE EXTENSION IF NOT EXISTS pg_cron;
 -- Schedule the job for Midnight EST  every Sunday (day 0)
 SELECT cron.schedule(
     'weekly-shark-reset',   -- The name of the cron job
-    '0 0 * * 0',            -- Cron expression: Minute(0) Hour(0) DayOfMonth(*) Month(*) DayOfWeek(0=Sunday)
+    '0 4 * * 0',            -- Cron expression: Minute(0) Hour(0) DayOfMonth(*) Month(*) DayOfWeek(0=Sunday)
     $$ SELECT process_weekly_shark_reset(); $$
 );
